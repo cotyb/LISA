e1147a471c4b03360f2004a8a186e1bbc9ee1f72
cotyb <xieleicotyb@gmail.com>
2016-01-27 11:26:17 +0800

    transfer flow rule to five tuples
    cotyb, practices make expert

---
+++ transfer flow rule to five tuplea.py
@@ -0,0 +1,23 @@
+#coding = ""utf-8
+import os
+files = os.listdir("flows-h100-copy")
+#result = os.mkdir("result")
+print files
+for file in files:
+    #print type(file)
+    fp = open("flows-h100-copy/" + file,"r")
+    fw = open("result/" + file,"w")
+    for flow in fp.readlines()[1:]:
+        if flow.__contains__("4294967293"):
+            continue
+        try:
+            flow = eval(flow)
+        except:
+            print file
+            print flow
+        if flow[0]["match"].has_key("nw_src"):
+            print >> fw,"src_ip:{0}, dst_ip:{1}, in_port:{2}, actions:{3}".format(flow[0]["match"]["nw_src"],\
+                                            flow[0]["match"]["nw_dst"], flow[0]["match"]["in_port"], flow[0]["actions"])
+    fp.close()
+    fw.close()
+
