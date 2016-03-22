788fe3c181512f341b0e6253dffd89365fc0d64b
cotyb <xieleicotyb@gmail.com>
2016-03-22 16:50:45 +0800

    multi controller
    cotyb

--- LISA/sample_topo/sample_topo.py
+++ LISA/sample_topo/sample_topo.py
@@ -14,9 +14,12 @@
     Description: Load topology in Mininet
     Author: XieLei (xieleicotyb@126.com)  2016/3/13
 '''
+
+from argparse import ArgumentParser
+from socket import gethostbyname
 from os import getuid
 
-from mininet.log import lg, info, setLogLevel
+from mininet.log import lg, info
 from mininet.cli import CLI
 from mininet.net import Mininet
 from mininet.topo import Topo
@@ -24,10 +27,9 @@ from mininet.link import Link, Intf
 from mininet.node import Host, OVSKernelSwitch, Controller, RemoteController
 
 class LisaTopo( Topo ):
-    "Topology for LISA"
 
     def __init__( self ):
-        #super(LisaTopo, self).__init__()
+	    #super(LisaTopo, self).__init__()
         Topo.__init__(self)
 
         # Set topology info
@@ -35,30 +37,60 @@ class LisaTopo( Topo ):
         switches = [1, 2, 3, 4]
         links = [{(1, 2):(2, 1)}, {(1, 3):(3, 1)}, {(2, 3):(2, 3)}, {(2, 4):(3, 1)}, {(3, 4):(2, 3)}]
         links_sw_host = [{(1, 1):(1, 1)}, {(2, 4):(1, 2)}]
-		
 
+        # add switches and hosts
+        self.sw_obj = []
+	for s in switches:
+            sw = self.addSwitch("s%s" %s)
+	    self.sw_obj.append(sw)
+
+        host_obj = []
+	for h in hosts:
+            host = self.addHost("h%s" %h)
+	    host_obj.append(host)
         # Wire up switches       
         for link in links:
-            s1 = self.addSwitch("s%s" % link.keys()[0][0])
-	    s2 = self.addSwitch("s%s" % link.keys()[0][1])
-            self.addLink(s1, s2 , link.values()[0][0], link.values()[0][1])
+            self.addLink(self.sw_obj[link.keys()[0][0]-1], self.sw_obj[link.keys()[0][1]-1] , link.values()[0][0], link.values()[0][1])
         
         # Wire up hosts
         for link in links_sw_host:
-            h1 = self.addHost("h%s" % link.keys()[0][0])
-	    s2 = self.addSwitch("s%s" % link.keys()[0][1])
-            self.addLink(h1, s2 , link.values()[0][0], link.values()[0][1])
+            self.addLink(host_obj[link.keys()[0][0]-1], self.sw_obj[link.keys()[0][1]-1] , link.values()[0][0], link.values()[0][1])
 
-                
-       
-def LisaTopoTest():
+        
+class LisaMininet ( Mininet ):
+
+    def build( self ):
+        super( LisaMininet, self ).build()
+
+
+def LisaTopoTest( controller_ip, controller_port,  controller_num ):
     topo = LisaTopo()
-    main_controller = lambda a:RemoteController(a, ip="localhost", port=6633)
-    net = Mininet( topo=topo, switch=OVSKernelSwitch, controller=main_controller)
+
+    #  main_controller = lambda a: RemoteController( a, ip=controller_ip, port=controller_port)
     
+    net = LisaMininet( topo=topo, switch=OVSKernelSwitch, controller=None)
+    controller_list = []
+    for i in range(controller_num):
+        name = "c%s" %i
+        c = net.addController(name, controller=RemoteController,port=controller_port+i)
+        controller_list.append(c)
+
+    for s in topo.sw_obj:
+        s = net.getNodeByName(s)
+        s.start(controller_list)
     
     net.start()
-    
+    '''
+    # These switches should be set to a local controller..
+    # dummy_switches = topo.dummy_switches
+    main_controller = lambda a: RemoteController(a, ip=controller_ip, port=controller_port)
+    main_controller = net.addController(name='main_controller', controller=main_controller)
+    main_controller.start()
+
+    dummyClass = lambda a: RemoteController( a, ip=dummy_controller_ip, port=dummy_controller_port)
+    dummy_controller = net.addController( name='dummy_controller', controller=dummyClass)
+    dummy_controller.start()
+    '''
         
     CLI( net )
     net.stop()
@@ -67,7 +99,33 @@ if __name__ == '__main__':
     if getuid()!=0:
         print "Please run this script as root / use sudo."
         exit(-1)
-    setLogLevel("info")
 
-    LisaTopoTest()
-    
+    lg.setLogLevel( 'info')
+    description = "Put Lisa backbone in Mininet"
+    parser = ArgumentParser(description=description)
+    parser.add_argument("-c", dest="controller_name",
+                      default="localhost",
+                      help="Controller's hostname or IP")
+    parser.add_argument("-p", dest="controller_port",type=int,
+                      default=6633,
+                      help="Controller's port")
+    '''
+    parser.add_argument("-c2", dest="dummy_controller_name",
+                      default="localhost",
+                      help="Dummy controller's hostname or IP")
+    parser.add_argument("-p2", dest="dummy_controller_port",type=int,
+                      default=6634,
+                      help="Dummy ontroller's port")
+    '''
+    parser.add_argument("-num", dest="controller_number",type=int,
+                      default=2,
+                      help="the number of the controller")
+    args = parser.parse_args()
+    print description
+    print "Starting with primary controller %s:%d" % (args.controller_name, args.controller_port)
+    # print "Starting with dummy controller %s:%d" % (args.dummy_controller_name, args.dummy_controller_port)
+    print "starting %s controllers in this network" %args.controller_number
+    Mininet.init()
+    LisaTopoTest(gethostbyname(args.controller_name), args.controller_port, args.controller_number)
+
+topos = { 'mininetbuilder': ( lambda: LisaTopo() ) }
